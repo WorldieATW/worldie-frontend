2ec756d495c4f411ed2d36c199341d7976c7065b
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-12-01 21:34:56 +0700

    bootstrapped repo

---
+++ src/components/elements/Header/index.tsx
@@ -0,0 +1,7 @@
+import Head from 'next/head'
+
+export const Header = () => (
+  <Head>
+    <title>Worldie</title>
+  </Head>
+)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import { Header } from '@elements'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <main className="w-full min-h-screen ">
+        <section className="max-w-[1440px] flex mx-auto">
+          <Component {...pageProps} />
+        </section>
+      </main>
+    </>
+  )
 }
