11c0ee1116de6707325e4947816dae70ea6e2f65
Aidah Novallia Putri <aidahptr15@gmail.com>
2023-12-04 17:42:24 +0700

    prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,14 +10,14 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        'flash': '#F6F8FA',
-        'royal': '#4468E2',
-        'grayjoy': '#828282'
+        flash: '#F6F8FA',
+        royal: '#4468E2',
+        grayjoy: '#828282',
       },
       fontFamily: {
-        'paytone': ['Paytone One', 'sans-serif'],
-        'poppins': ['Poppins', 'sans-serif']
-      }
+        paytone: ['Paytone One', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
     },
   },
   plugins: [],
