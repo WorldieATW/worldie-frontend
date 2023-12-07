6ef3b3d0542b37c6b03b2c0caac876ac85648760
Aidah Novallia Putri <aidahptr15@gmail.com>
2023-12-07 10:08:01 +0700

    add AsetUsahaCard element

---
+++ src/components/elements/AsetUsahaCard/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Image from 'next/image'
+import { IoLocationSharp } from 'react-icons/io5'
+import { AsetUsahaCardProps } from './interface'
+import { GiTransportationRings } from "react-icons/gi";
+
+export const AsetUsahaCard: React.FC<AsetUsahaCardProps> = ({
+  nama,
+  harga,
+  imgUrl,
+  provinsi,
+  negara,
+  jenisKendaraan,
+}) => {
+  return (
+    <div className="w-52 h-52 rounded-lg shadow font-poppins hover:scale-105 hover:shadow-lg">
+      <div className="flex justify-center">
+        <Image
+          alt={nama}
+          src={imgUrl}
+          width={210}
+          height={210}
+          className="rounded-lg"
+        />
+      </div>
+      <div className="p-4 gap-y-1 flex flex-col text-black">
+        <div className="flex items-center gap-x-2">
+          {jenisKendaraan ? (
+            <>
+              <GiTransportationRings size={15} className="fill-grayjoy/80" />
+              <span className="text-xs text-grayjoy/80">{jenisKendaraan}</span>
+            </>
+          ) : (
+            <>
+              <IoLocationSharp size={10} className="fill-grayjoy/80" />
+              <span className="text-xs text-grayjoy/80">
+                {provinsi}, {negara}
+              </span>
+            </>
+          )}
+        </div>
+        <span className="font-bold text">Hotel Kemangi</span>
+        <span className="text-xs text-grayjoy italic">
+          Rp{harga} each night
+        </span>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/elements/AsetUsahaCard/interface.ts
@@ -0,0 +1,8 @@
+export interface AsetUsahaCardProps {
+  nama: string
+  harga: number
+  imgUrl: string
+  provinsi?: string
+  negara?: string
+  jenisKendaraan?: string
+}
