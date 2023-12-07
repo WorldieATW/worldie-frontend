ae65d0d55568604be81297bd37fc9fe805167252
Aidah Novallia Putri <aidahptr15@gmail.com>
2023-12-07 17:25:54 +0700

    finish accomodation page

--- src/components/modules/PenginapanModule/constant.ts
+++ src/components/modules/PenginapanModule/constant.ts
@@ -1,1 +1,22 @@
-export const VAR_NAME = ''
+export const JENIS_PENGINAPAN_OPTIONS = [
+  {
+    name: 'All',
+    value: 'ALL',
+  },
+  {
+    name: 'Hotel',
+    value: 'HOTEL',
+  },
+  {
+    name: 'Villa',
+    value: 'VILLA',
+  },
+  {
+    name: 'Kost',
+    value: 'KOST',
+  },
+  {
+    name: 'Kontrakan',
+    value: 'KONTRAKAN',
+  },
+]
--- src/components/modules/PenginapanModule/index.tsx
+++ src/components/modules/PenginapanModule/index.tsx
@@ -1,14 +1,46 @@
-import React from 'react'
-// import {HeroSection, FAQSection} from './sections
-// import {} from './module-elements'
+import { AsetUsahaCard, BackButton, Select } from "@elements"
+import { useAsetUsahaApi } from "src/components/hooks/useAsetUsahaApi"
+import { JENIS_PENGINAPAN_OPTIONS } from "./constant"
+
 
 export const PenginapanModule: React.FC = () => {
-  // TODO: Write module's logic
+  const { asetUsaha, setJenisPenginapan } = useAsetUsahaApi({
+    initialTipe: 'PENGINAPAN',
+  })
 
   return (
-    <>
-      {/* <HeroSection></HeroSection> */}
-      {/* <FAQSection></FAQSection> */}
-    </>
+    <section className="w-full px-7 flex flex-col gap-y-8">
+      <BackButton text="Accomodation" />
+
+      <div className="flex justify-end">
+        <Select
+          options={JENIS_PENGINAPAN_OPTIONS}
+          onChange={(value) => {
+            if (value === 'ALL') {
+              setJenisPenginapan('')
+            } else {
+              setJenisPenginapan(value)
+            }
+          }}
+        />
+      </div>
+
+      <div className="flex flex-wrap gap-6">
+        {asetUsaha.map((aset, index) => {
+          console.log(aset)
+          return (
+            <AsetUsahaCard
+              key={index}
+              nama={aset.nama}
+              harga={aset.harga}
+              imgUrl={aset.imgUrl}
+              provinsi={aset.alamat?.provinsi}
+              negara={aset.alamat?.negara}
+              detailLink=""
+            />
+          )
+        })}
+      </div>
+    </section>
   )
 }
