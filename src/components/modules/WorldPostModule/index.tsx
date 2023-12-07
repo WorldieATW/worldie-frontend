2898fdabe7964cb41197f8ec09ccf2c9aaf9cbff
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-12-07 13:31:50 +0700

    improve: padding

--- src/components/modules/WorldPostModule/index.tsx
+++ src/components/modules/WorldPostModule/index.tsx
@@ -1,3 +1,10 @@
 export const WorldPostModule = () => {
-  return <div></div>
+  return (
+    <>
+      <div className="flex flex-col">
+        <h1 className="font-paytone text-lg px-7">Home</h1>
+        <hr />
+      </div>
+    </>
+  )
 }
