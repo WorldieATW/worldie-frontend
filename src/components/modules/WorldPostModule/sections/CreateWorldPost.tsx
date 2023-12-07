130b4368733a7a5634e4000a1fa56e4ac4ade7fd
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-12-07 13:57:20 +0700

    added chakra ui

---
+++ src/components/modules/WorldPostModule/sections/CreateWorldPost.tsx
@@ -0,0 +1,12 @@
+import { HiUserCircle } from "react-icons/hi2"
+
+export const CreateWorldPost = () => {
+    return (
+        <div className="px-7 flex flex-row">
+            <HiUserCircle fill="black" size={45} />
+            <div className="flex flex-col">
+                {/* <TextField /> */}
+            </div>
+        </div>
+    )
+}
