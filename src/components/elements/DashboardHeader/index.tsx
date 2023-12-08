b4fc0a8590393d577b8f12765068d75d6f143310
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2023-12-08 14:00:01 +0700

    change dashboard header style

--- src/components/elements/DashboardHeader/index.tsx
+++ src/components/elements/DashboardHeader/index.tsx
@@ -5,14 +5,14 @@ export const DashboardHeader = () => {
 
   return (
     <section className="flex flex-col gap-4 lg:gap-6 px-7 pb-4 lg:pb-12">
-      <h1 className="font-poppins font-bold text-lg lg:text-3xl">
+      <h1 className="font-paytone text-lg lg:text-2xl">
         {user?.nama}'s Dashboard
       </h1>
 
-      <span className="font-poppins font-bold text-lg lg:text-3xl pt-4 lg:pt-12">
+      <span className="font-paytone text-lg lg:text-2xl pt-4 lg:pt-8">
         Welcome, {user?.nama}!
       </span>
-      <span className="font-semibold">Semoga aktivitas Anda menyenangkan </span>
+      <span className="font-poppins">Semoga aktivitas Anda menyenangkan </span>
     </section>
   )
 }
--- src/components/modules/DashboardAdminModule/index.tsx
+++ src/components/modules/DashboardAdminModule/index.tsx
@@ -2,7 +2,7 @@ import { DashboardHeader } from '@elements'
 
 export const DashboardAdminModule = () => {
   return (
-    <section className="flex flex-col py-12">
+    <section className="flex flex-col py-6 lg:py-8">
       <DashboardHeader />
       <div className="border border-b border-black/5" />
     </section>
