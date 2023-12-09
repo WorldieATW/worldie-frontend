66a5514d9284b275205952b1c5984591ce0a8435
Joselin Permata Aprillia <103546691+joselinprmt@users.noreply.github.com>
2023-12-09 19:33:56 +0700

    comment section: add picture when no comments yet

--- src/components/modules/DetailWorldPostModule/sections/Comments.tsx
+++ src/components/modules/DetailWorldPostModule/sections/Comments.tsx
@@ -1,11 +1,12 @@
 import { BsChatLeftDots } from 'react-icons/bs'
 import { CommentsProps } from '../interface'
 import { CommentCard } from '../module-elements'
+import { Image, Skeleton } from '@chakra-ui/react'
 
 export const Comments: React.FC<CommentsProps> = ({
   comments,
-  // commentsChanged,
-  // setCommentsChanged,
+  //commentsChanged,
+  //setCommentsChanged,
 }) => {
   return (
     <section className="flex flex-col gap-3">
@@ -15,17 +16,26 @@ export const Comments: React.FC<CommentsProps> = ({
       </div>
 
       {comments ? (
-        comments.map((worldPost) => (
-          <CommentCard
-            key={worldPost.id}
-            worldPost={worldPost}
-            // commentsChanged={commentsChanged}
-            // setCommentsChanged={setCommentsChanged}
-            isDetail={false}
-          />
-        ))
+        (comments.length > 0) ? (
+          comments.map((worldPost) => (
+            <CommentCard
+              key={worldPost.id}
+              worldPost={worldPost}
+              //commentsChanged={commentsChanged}
+              //setCommentsChanged={setCommentsChanged}
+              isDetail={false}
+            />
+          ))
+        ) : (
+          <div className="flex flex-col gap-3 items-center py-7">
+            <Image src="/no-comment-yet.png" boxSize="200px" />
+            <p className="font-semibold text-m">No comments yet. Start the conversation!</p>
+          </div>
+        )
       ) : (
-        <div className="px-7">{/* TODO: ketika belum ada komenn */}</div>
+        <div className="px-7">
+          <Skeleton height={400} />
+        </div>
       )}
     </section>
   )
