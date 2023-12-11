8f0c5e9fd6c410590ba1226d5cf6d77f454c23a2
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2023-12-11 14:37:02 +0700

    add destinations

--- src/components/modules/LandingModule/index.tsx
+++ src/components/modules/LandingModule/index.tsx
@@ -1,9 +1,11 @@
+import { DestinationSection } from './sections/DestinationSection'
 import { HeroSection } from './sections/HeroSection'
 
 export const LandingModule = () => {
   return (
     <section className="text-center flex flex-col py-8">
       <HeroSection />
+      <DestinationSection />
     </section>
   )
 }
