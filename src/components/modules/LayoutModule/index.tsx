532a68a330ffdca4608be6d602ce3354ee5f10e6
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-12-11 20:13:36 +0700

    removed handle protected page

--- src/components/modules/LayoutModule/index.tsx
+++ src/components/modules/LayoutModule/index.tsx
@@ -1,28 +1,10 @@
-import React, { ComponentPropsWithoutRef, useEffect } from 'react'
+import React, { ComponentPropsWithoutRef } from 'react'
 import { LeftSidebar, RightSidebar } from './module-elements'
-import { LayoutContextProvider, useAuthContext } from '@contexts'
-import toast from 'react-hot-toast'
-import { useRouter } from 'next/router'
+import { LayoutContextProvider } from '@contexts'
 
 export const LayoutModule: React.FC<ComponentPropsWithoutRef<'main'>> = ({
   children,
 }) => {
-  const router = useRouter()
-  const { isAuthenticated } = useAuthContext()
-
-  const handleProtectedPage = () => {
-    if (isAuthenticated) return
-
-    router.push('/login')
-    toast.error('Must be logged in to access this page')
-  }
-
-  useEffect(() => {
-    handleProtectedPage()
-  }, [])
-
-  if (!isAuthenticated) return <></>
-
   return (
     <main className="flex justify-center bg-white">
       <LayoutContextProvider>
